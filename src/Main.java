7765bd7ec8d68d110e73a59491aa2a4e27a10f04
hojooglim <hojooglim@gmail.com>
2023-06-27 09:19:02 +0900

    서울에서 김서방 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        String[] seoul = {"Jain", "kim"};
+        String answer;
+
+        int idx = 0;
+        String kim = "kim";
+
+        for (int i = 0; i < seoul.length ; i++) {
+            if(kim.equals(seoul[i])){
+                idx=i;
+                break;
+            }
+        }
+        answer = "김서방은 "+idx+"에 있다";
+        System.out.println(answer);
+
 
     }
 }
