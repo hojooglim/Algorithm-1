f9259dec8c158797f2bd0e1e97607f15007b27c3
hojooglim <hojooglim@gmail.com>
2023-06-28 09:30:29 +0900

    음양 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+
 public class Main {
     public static void main(String[] args) {
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};
+        int sum = 0;
+        for (int i = 0; i <absolutes.length ; i++) {
+            if (signs[i]){
+                sum += absolutes[i];
+            } else {
+                sum +=(absolutes[i]*-1);
+            }
+        }
+
 
     }
+
 }
