b725c26f625a920f4cf327c6ae7b5bbe035501e1
ChuJH <09strawberry@naver.com>
2023-06-28 10:43:34 +0900

    음양 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+//      [음양 더하기]
+        int[] absolutes = {4, 7, 12};
+        boolean[] signs = {true,false,true};
+//      ------------------------------------
+        int answer = 0;
+
+        for(int j=0; j<signs.length; j++){
+//          삼항연산자 사용
+            answer = signs[j] ? (answer += absolutes[j]) : (answer -= absolutes[j]);
+        }
+        System.out.println(answer);
 
     }
 }
