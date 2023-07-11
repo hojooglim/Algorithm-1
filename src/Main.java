e0bae7b9ef691dd2440b64c2520589c7b0af6a58
ChuJH <09strawberry@naver.com>
2023-07-11 19:06:43 +0900

    두 개 뽑아서 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {2, 1, 3, 4, 1};
+//      [두 개 뽑아서 더하기]
+//      -------------------------------------
+        HashSet<Integer> num = new HashSet<>();
 
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                num.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        ArrayList<Integer> arr = new ArrayList<>(num);
+        Collections.sort(arr);
+
+        // int[] 타입으로 변환
+        int[] answer = new int[arr.size()];
+        for(int i=0; i<arr.size(); i++){
+            answer[i] += arr.get(i);
+        }
+        System.out.println(answer);
     }
 }
