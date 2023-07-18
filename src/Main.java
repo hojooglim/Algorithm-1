9d45cab52a56edea270003ca3fab482e5cf68989
ChuJH <09strawberry@naver.com>
2023-07-18 19:38:47 +0900

    수박수박수박수박수박수

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+//      [수박수박수박수박수박수?]
+        int n = 3;
+//      --------------------------------
+        String answer = "";
 
+        for(int i=0; i<n; i++){
+            answer += (i%2==0) ? "수" : "박";
+        }
+        System.out.println(answer);
     }
 }
